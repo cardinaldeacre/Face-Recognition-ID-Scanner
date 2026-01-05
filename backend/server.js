3bbc2b926876f7af9b9fcd58f270677b10dc8f03
andrian maulana <134795751+AndreanMlna@users.noreply.github.com>
2026-01-05 11:08:02 +0700

    Fix port to 8000 and relax CORS for testing

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 8000; 
 
 const swaggerUi = require('swagger-ui-express');
 const { swaggerSpec } = require('./swagger');
